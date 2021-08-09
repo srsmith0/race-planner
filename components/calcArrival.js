898c5ca00a02809f31182473564a4c824fc357da
Shawn Smith <srsmith11@live.com>
2021-08-09 17:02:15 -0600

    start time calculates correct format

--- components/calcArrival.js
+++ components/calcArrival.js
@@ -1,22 +1,32 @@
 export default function calcArrival({ aid, pace, distance, segmentTime, lastArrival }) {
   if (aid.location === "Start") {
     //sets start arrival time to start time or defaults to nothing if value is empty
-    return aid.arrivalTime = document.getElementById('startTime').value || " ";
+    let startTime = document.getElementById('startTime').value;
+    startTime = startTime[0] === "0" ? startTime.slice(1) : startTime;
+    return aid.arrivalTime = `${determineAmPm(startTime)}`;
   } else {
     //convert pace to minutes/mile
     let arrivalHour = parseInt(lastArrival.split(':')[0]) + parseInt(segmentTime.split(':')[0]);
     let arrivalMinute = parseInt(lastArrival.split(':')[1]) + parseInt(segmentTime.split(':')[1]);
-    if (arrivalMinute >= 60) {
+    let arrival
+    if (arrivalMinute > 59) {
+      console.log('greater than 59')
       const convertedHours = Math.floor(arrivalMinute / 60);
       const convertedMinutes = (arrivalMinute / 60).toString().split('.')[1] * 6;
-      console.log(convertedMinutes)
-      console.log(convertedHours)
+      console.log(arrivalHour, convertedHours)
+      return arrival = `${arrivalHour + convertedHours}:${convertedMinutes}`
     }
-    console.log(arrivalMinute)
-    const arrival = `${arrivalHour}:${arrivalMinute}`;
+    arrival = `${arrivalHour}:${arrivalMinute}`;
     //multiply that by distance
     //convert that to hours and minutes
     return aid.arrivalTime = arrival
+  }
+
+  function determineAmPm(time) {
+    let hours = parseInt(time.split(':')[0])
+    let amPm = hours >= 12 ? ' PM' : ' AM';
+    hours = (hours % 12) || 12;
+    return `${hours}:${time.split(':')[1]}${amPm}`
   }
 }
 // function padLeadingZeros(num, size) {
