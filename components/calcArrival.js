f3d1a94d73e4e867bf6316660474006faac301a7
Shawn Smith <srsmith11@live.com>
2021-08-10 20:04:09 -0600

    added 24 hour conversion

--- components/calcArrival.js
+++ components/calcArrival.js
@@ -10,8 +10,10 @@ export default function calcArrival({ aid, pace, distance, segmentTime, lastArri
     let arrivalHour = parseInt(lastArrival.split(':')[0]) + parseInt(segmentTime.split(':')[0]);
     let arrivalMinutes = parseInt(lastArrival.split(':')[1]) + parseInt(segmentTime.split(':')[1]);
     //set aid.arrivalTime to 24 hour time
-    aid.arrivalTime = `${arrivalHour}:${arrivalMinutes}`;
+    let aidArrivalTime =convert24PlusHours(arrivalHour, arrivalMinutes)
+      // added function, this used to be arrivaltime`${arrivalHour}:${arrivalMinutes}`;
     //set 24 hr time to 12 hr time to display in table
+    arrivalHour = arrivalHour > 25 ? arrivalHour - 24 : arrivalHour;
     let amPm = arrivalHour >= 12 ? ' PM' : ' AM';
     let arrivalTweleveHour = (arrivalHour % 12) || 12;
     let arrival = `${arrivalTweleveHour}:${arrivalMinutes}` + amPm;
@@ -20,11 +22,13 @@ export default function calcArrival({ aid, pace, distance, segmentTime, lastArri
       let convertedMinutes = Math.round(((arrivalMinutes / 60).toFixed(2).split('.')[1] / 100) * 60);
       convertedMinutes = convertedMinutes.toString().length === 1 ? convertedMinutes + '0' : convertedMinutes;
       //problem is when it goes from 12 PM to 1 PM
-      aid.arrivalTime = `${arrivalHour + convertedHours}:${convertedMinutes}`;
+      aidArrivalTime = `${arrivalHour + convertedHours}:${convertedMinutes}`;
       arrival = `${arrivalTweleveHour + convertedHours}:${convertedMinutes}` + amPm;
     } else {
       arrival;
     }
+    aid.arrivalTime = aidArrivalTime;
+    //added this line to assign time to aid object.  used to assign object above if block and in if block
     return arrival;
   };
 
@@ -38,4 +42,14 @@ export default function calcArrival({ aid, pace, distance, segmentTime, lastArri
       return `${hours}:${time.split(':')[1]}` + amPm
     }
   }
+
+  function convert24PlusHours(hours, minutes) {
+    let time
+    if (hours > 25) {
+      time = `${hours - 24}:${minutes}`;
+    } else {
+      time = `${hours}:${minutes}`;
+    }
+    return time
+  };
 };
