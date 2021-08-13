6c7e31b21a672d19f6ffce0f0ff2bd5dd16b8a50
Shawn Smith <srsmith11@live.com>
2021-08-13 05:57:57 -0600

    made tableRow component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import { useState } from 'react'
-import dynamic from 'next/dynamic'
 
-const DynamicCalcArrival = dynamic(
-  () => import('../components/calcArrival'),
-  { ssr: false }
-)
+import AidStationRow from '../components/AidStationRow';
 
 export default function Home() {
   //change state when ready to ship
@@ -17,7 +13,12 @@ export default function Home() {
   const [calorieRate, setCalorieRate] = useState(250);
   const [hydrationRate, setHydrationRate] = useState(750);
   const [sodiumRate, setSodiumRate] = useState(500);
-  const [aidStations, setAidStations] = useState([{ location: "Start", distance: 0, segmentTime: "", arrivalTime: "" }]);
+  const [aidStations, setAidStations] = useState([{
+    location: "Start",
+    distance: 0,
+    segmentTime: "",
+    arrivalTime: "",
+  }]);
   const [plan, setPlan] = useState({
     ascent: "",
     descent: "",
@@ -72,102 +73,6 @@ export default function Home() {
     const updatedAidStations = [...aidStations, newAidStation];
     setAidStations(updatedAidStations)
   }
-
-  function createAidTableRow(aid, index) {
-    //sets aid segment distance
-    aid.location === "Start" ? aid.segmentDistance = 0 : aid.segmentDistance = aid.distance - aidStations[index - 1].distance;
-
-    const getSegmentTime = () => {
-      if (aid.location === "Start" || plan.pace === "") {
-        return ""
-      } else {
-        //splits pace to minutes and seconds to convert to time
-        const time = getPaceToMinutes(plan.pace);
-        getNutritionFactor(time)
-        return aid.segmentTime = time;
-      }
-    }
-
-    function getPaceToMinutes(pace) {
-      //separate minutes from seconds from pace
-      const minutes = pace.split(':')[0];
-      const seconds = pace.split(':')[1];
-      //uses segment distance and pace minutes/seconds to calculate time
-      let totalMinutes = aid.segmentDistance * minutes;
-      const totalSeconds = aid.segmentDistance * seconds;
-      //convert seconds to minutes
-      const convertedSeconds = totalSeconds.toFixed(2) / 60;
-      if (totalSeconds <= 59) {
-        //seconds are less than a minute => return with 1 minute added
-        totalMinutes++;
-      } else {
-        totalMinutes = totalMinutes + Math.round(convertedSeconds);
-      }
-
-      function formatTotalMinutes(minutes) {
-        if (minutes < 10) {
-          return '0' + totalMinutes;
-        } else if (minutes.toString().length === 1) {
-          return totalMinutes + '0';
-        } else {
-          return totalMinutes
-        }
-      }
-
-      let convertedTime = (totalMinutes / 60).toFixed(1).toString();
-      //this used to be in the return line.  Made variables so I can create if block
-      let convertedHours = convertedTime.split('.')[0];
-      let convertedMinutes = convertedTime.split('.')[1] * 6;
-
-      if (parseInt(convertedMinutes) < 9) {
-        convertedMinutes = '0' + convertedMinutes;
-      } else if (convertedMinutes.length === 1) {
-        convertedMinutes = convertedMinutes + '0';
-      } else {
-        convertedMinutes
-      }
-      
-      return totalMinutes / 60 <= 1 ? `00:${formatTotalMinutes(totalMinutes)}` : `${convertedHours}:${convertedMinutes}`
-    }
-
-    function getNutritionFactor(time) {
-      if (time === "") {
-        return 0
-      } else {
-        let number = time.toString().split(':')[0];
-        let minutes = time.toString().split(':')[1];
-        let convertedMinutes = (minutes / 60).toFixed(2);
-        return parseInt(number) + parseFloat(convertedMinutes);
-      }
-    }
-    
-    getSegmentTime()
-    return (
-      <tr key={index + aid.location}>
-        <td>{aid.location}</td>
-        <td>{aid.distance}</td>
-        <td>{aid.segmentDistance}</td>
-        <td>{aid.segmentTime}</td>
-        <td><DynamicCalcArrival
-          pace={plan.pace}
-          aid={aid}
-          segmentTime={aid.segmentTime}
-          lastArrival={index === 0 ? "" : aidStations[index - 1].arrivalTime}
-        />
-        </td>
-        <td>{aid.cutoff}</td>
-        <td>{Math.round(getNutritionFactor(aid.segmentTime) * calorieRate)} cal</td>
-        <td>{Math.round(getNutritionFactor(aid.segmentTime) * hydrationRate)} mL</td>
-        <td>{Math.round(getNutritionFactor(aid.segmentTime) * sodiumRate)} mg</td>
-        <td>{aid.crew}</td>
-        <td>{aid.pacer}</td>
-        <td>{aid.dropBag}</td>
-        <td>{aid.waterOnly}</td>
-        <td className="comments">{aid.comments}</td>
-      </tr>
-    )
-  }
-  
 
   return (
     <div>
@@ -404,7 +309,16 @@ export default function Home() {
             </tr>
          </thead>
           <tbody>
-          {aidStations.map((aid, index) => createAidTableRow(aid, index))}
+            {aidStations.map((aid, index) => <AidStationRow
+              key={aid.location + index}
+              aid={aid}
+              index={index}
+              aidStations={aidStations}
+              pace={plan.pace}
+              calorieRate={calorieRate}
+              hydrationRate={hydrationRate}
+              sodiumRate={sodiumRate}
+            />)}
           </tbody>
       </table>
 
