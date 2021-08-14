044db69c914e6a17e14235f2201a3ef4f71241ff
Shawn Smith <srsmith11@live.com>
2021-08-14 10:17:20 -0600

    fixed time output bugs in aid table

--- components/AidStationRow.js
+++ components/AidStationRow.js
@@ -11,17 +11,19 @@ export default function createAidTableRow({
   aid, index, aidStations, pace, calorieRate, hydrationRate, sodiumRate
 }) {
     //sets aid segment distance
-    aid.location === "Start" ? aid.segmentDistance = 0 : aid.segmentDistance = parseFloat(aid.distance - aidStations[index - 1].distance).toFixed(2);
-    const getSegmentTime = () => {
-      if (aid.location === "Start" || pace === "") {
-        return ""
-      } else {
-        //splits pace to minutes and seconds to convert to time
-        const time = getPaceToMinutes(pace);
-        getNutritionFactor(time)
-        return aid.segmentTime = time;
-      }
-    }
+  aid.location === "Start" ? aid.segmentDistance = 0 : aid.segmentDistance = parseFloat(aid.distance - aidStations[index - 1].distance).toFixed(2);
+  //if no float, converts distance to integer
+  aid.segmentDistance = aid.segmentDistance.toString().split('.')[1] === "00" ? aid.segmentDistance.split('.')[0] : aid.segmentDistance;
+  const getSegmentTime = () => {
+    if (aid.location === "Start" || pace === "") {
+      return ""
+    } else {
+      //splits pace to minutes and seconds to convert to time
+      const time = getPaceToMinutes(pace);
+      getNutritionFactor(time)
+      return aid.segmentTime = time;
+    };
+  };
 
     function getPaceToMinutes(pace) {
       //separate minutes from seconds from pace
@@ -37,7 +39,7 @@ export default function createAidTableRow({
         totalMinutes++;
       } else {
         totalMinutes = totalMinutes + Math.round(convertedSeconds);
-      }
+      };
 
       function formatTotalMinutes(minutes) {
         if (minutes < 10) {
@@ -47,8 +49,8 @@ export default function createAidTableRow({
         } else {
           //has to round to avoid long decimals in time output for aid station '0.00' format
           return Math.round(totalMinutes)
-        }
-      }
+        };
+      };
 
       let convertedTime = (totalMinutes / 60).toFixed(1).toString();
       //this used to be in the return line.  Made variables so I can create if block
@@ -60,10 +62,10 @@ export default function createAidTableRow({
         convertedMinutes = convertedMinutes + '0';
       } else {
         convertedMinutes
-      }
+      };
       
       return totalMinutes / 60 <= 1 ? `00:${formatTotalMinutes(totalMinutes)}` : `${convertedHours}:${convertedMinutes}`
-    }
+    };
 
     function getNutritionFactor(time) {
       if (time === "") {
@@ -73,8 +75,8 @@ export default function createAidTableRow({
         let minutes = time.toString().split(':')[1];
         let convertedMinutes = (minutes / 60).toFixed(2);
         return parseInt(number) + parseFloat(convertedMinutes);
-      }
-    }
+      };
+    };
     
     getSegmentTime()
     return (
--- utils/AidArrivalTime.js
+++ utils/AidArrivalTime.js
@@ -9,26 +9,31 @@ export default function calcArrival({ aid, segmentTime, lastArrival }) {
     let arrivalHour = parseInt(lastArrival.split(':')[0]) + parseInt(segmentTime.split(':')[0]);
     let arrivalMinutes = Math.round(parseInt(lastArrival.split(':')[1]) + parseInt(segmentTime.split(':')[1]));
     //set aid.arrivalTime to 24 hour time
-    let aidArrivalTime = convert24PlusHours(arrivalHour, arrivalMinutes);
     //set 24 hr time to 12 hr time to display in table
+    let aidArrivalTime = determineHourIncrease(arrivalHour, arrivalMinutes);
+    aidArrivalTime = convert24PlusHours(aidArrivalTime);
     arrivalHour = arrivalHour > 24 ? arrivalHour - 24 : arrivalHour;
-    let amPm = arrivalHour === 24 || arrivalHour <= 12 ? ' AM' : ' PM';
+    let amPm = arrivalHour === 24 || arrivalHour < 12 ? ' AM' : ' PM';
     let arrivalTweleveHour = (arrivalHour % 12) || 12;
     //formats minutes to display correctly
     arrivalMinutes = arrivalMinutes.toString().length === 1 && arrivalMinutes < 10 ? '0' + arrivalMinutes : arrivalMinutes;
     let arrival = `${arrivalTweleveHour}:${arrivalMinutes}` + amPm;
+    console.log(arrival)
     if (arrivalMinutes > 59) {
       let convertedHours = Math.floor(arrivalMinutes / 60);
       let convertedMinutes = Math.round(((arrivalMinutes / 60).toFixed(2).split('.')[1] / 100) * 60);
-      convertedMinutes = convertedMinutes.toString().length === 1 && convertedMinutes < 9 ? '0' + convertedMinutes : convertedMinutes;
+      convertedMinutes = convertedMinutes.toString().length === 1 && convertedMinutes < 10 ? '0' + convertedMinutes : convertedMinutes;
       aidArrivalTime = `${arrivalHour + convertedHours}:${convertedMinutes}`;
       amPm = (arrivalHour + convertedHours) === 24 || (arrivalHour + convertedHours) <= 12 ? ' AM' : ' PM';
-      arrival = `${arrivalTweleveHour + convertedHours}:${convertedMinutes}` + amPm;
+      //converts to 12 hour if hour is > 12
+      arrivalTweleveHour = (arrivalTweleveHour + convertedHours) % 12 || 12;
+      arrival = `${arrivalTweleveHour}:${convertedMinutes}` + amPm;
     } else {
       arrival;
-    }
+    };
 
     aid.arrivalTime = aidArrivalTime;
+    console.log(aid)
     return arrival;
   };
 
@@ -42,14 +47,27 @@ export default function calcArrival({ aid, segmentTime, lastArrival }) {
       return `${hours}:${time.split(':')[1]}` + amPm;
     }
   };
-
-  function convert24PlusHours(hours, minutes) {
-    let time;
-    if (hours > 25) {
-      time = `${hours - 24}:${minutes}`;
+  function determineHourIncrease(arrivalHour, arrivalMinutes) {
+    let aidArrivalTime;
+    if (arrivalMinutes > 59) {
+      let convertedMinutesToHours = Math.floor(arrivalMinutes / 60);
+      arrivalMinutes = Math.round(((arrivalMinutes / 60).toFixed(2).split('.')[1] / 100) * 60);
+      arrivalMinutes = arrivalMinutes.toString().length === 1 && arrivalMinutes < 9 ? '0' + arrivalMinutes : arrivalMinutes;
+      return `${arrivalHour + convertedMinutesToHours}:${arrivalMinutes}`;
     } else {
-      time = `${hours}:${minutes}`;
+      return `${arrivalHour}:${arrivalMinutes}`;
+    };
+  };
+
+  function convert24PlusHours(time) {
+    let hours = time.split(':')[0];
+    let minutes = time.split(':')[1];
+    let convertedTime
+    if (hours > 25) {
+      convertedTime = `${hours - 24}:${minutes}`;
+    } else {
+      convertedTime = time;
     }
-    return time;
+    return convertedTime;
   };
 };
