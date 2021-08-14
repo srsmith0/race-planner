b790a272e1ca9613aa28edde525e052417209c4e
Shawn Smith <srsmith11@live.com>
2021-08-14 09:11:35 -0600

    added aid station table component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import { useState } from 'react'
 
-import AidStationRow from '../components/AidStationRow';
 import RaceInfoForm from '../components/RaceInfoForm';
 import AidStationForm from '../components/AidStationForm';
 import RaceInfo from '../components/RaceInfo';
+import AidStationTable from '../components/AidStationTable';
 
 export default function Home() {
   //change state when ready to ship
@@ -71,38 +71,13 @@ export default function Home() {
           setAidStations={setAidStations}
         />
 
-        <table className="default-text">
-          <thead>
-            <tr>
-              <th>Location</th>
-              <th>Distance</th>
-              <th>Segment Distance</th>
-              <th>Segment Time (hr:min)</th>
-              <th>Expected Arrival</th>
-              <th>Cuttoff Time</th>
-              <th>Calories/Segment</th>
-              <th>Liquid/Segment</th>
-              <th>Sodium/Segment</th>
-              <th>Crew Access</th>
-              <th>Pacer</th>
-              <th>Drop Bag</th>
-              <th>Water Only</th>
-              <th>Comments</th>
-            </tr>
-         </thead>
-          <tbody>
-            {aidStations.map((aid, index) => <AidStationRow
-              key={aid.location + index}
-              aid={aid}
-              index={index}
-              aidStations={aidStations}
-              pace={plan.pace}
-              calorieRate={calorieRate}
-              hydrationRate={hydrationRate}
-              sodiumRate={sodiumRate}
-            />)}
-          </tbody>
-      </table>
+        <AidStationTable
+          aidStations={aidStations}
+          plan={plan}
+          calorieRate={calorieRate}
+          hydrationRate={hydrationRate}
+          sodiumRate={sodiumRate}
+        />
 
       </main>
 
